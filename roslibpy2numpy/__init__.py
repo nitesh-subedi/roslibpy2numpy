2254b180d04ea0915d109b23f0293ebc637cde3f
nit-ash <subedinitesh43@gmail.com>
2023-03-22 22:52:03 +0545

    test_on_progress

--- roslibpy2numpy/__init__.py
+++ roslibpy2numpy/__init__.py
@@ -1,8 +1,7 @@
 """
-A module for converting ROS message types into numpy types, where appropriate
+A module for converting roslibpy message types to and from numpy types
 """
 
-from .registry import numpify, msgify
 from . import point_cloud2
 from . import image
 from . import occupancy_grid
--- roslibpy2numpy/geometry.py
+++ roslibpy2numpy/geometry.py
@@ -1,173 +1,128 @@
 import transformations as transformations
 import numpy as np
-
-
-class Transform:
-    def __init__(self, translation, rotation):
-        self.translation = translation
-        self.rotation = rotation
-
-    def __repr__(self):
-        return "Transform(translation={}, rotation={})".format(self.translation, self.rotation)
-
-
-class Vector3:
-    def __init__(self, x, y, z):
-        self.x = x
-        self.y = y
-        self.z = z
-
-    def __repr__(self):
-        return "Vector3(x={}, y={}, z={})".format(self.x, self.y, self.z)
-
-
-class Quaternion:
-    def __init__(self, x, y, z, w):
-        self.x = x
-        self.y = y
-        self.z = z
-        self.w = w
-
-    def __repr__(self):
-        return "Quaternion(x={}, y={}, z={}, w={})".format(self.x, self.y, self.z, self.w)
-
-
-class Point:
-    def __init__(self, x, y, z):
-        self.x = x
-        self.y = y
-        self.z = z
-
-    def __repr__(self):
-        return "Point(x={}, y={}, z={})".format(self.x, self.y, self.z)
-
-
-class Pose:
-    def __init__(self, position, orientation):
-        self.position = position
-        self.orientation = orientation
-
-    def __repr__(self):
-        return "Pose(position={}, orientation={})".format(self.position, self.orientation)
+import roslibpy
 
 
 def vector3_to_numpy(msg, hom=False):
     if hom:
-        return np.array([msg.x, msg.y, msg.z, 0])
+        return np.array([msg['x'], msg['y'], msg['z'], 0])
     else:
-        return np.array([msg.x, msg.y, msg.z])
+        return np.array([msg['x'], msg['y'], msg['z']])
 
 
 def numpy_to_vector3(arr):
-    if arr.shape[-1] == 4:
-        assert np.all(arr[..., -1] == 0)
-        arr = arr[..., :-1]
+    if arr.dtype != np.float64:
+        raise ValueError("Expected a floating point array")
 
-    if len(arr.shape) == 1:
-        return Vector3(**dict(zip(['x', 'y', 'z'], arr)))
-    else:
-        return np.apply_along_axis(
-            lambda v: Vector3(**dict(zip(['x', 'y', 'z'], v))), axis=-1,
-            arr=arr)
+    if arr.shape != (3, 1):
+        raise ValueError("Expected a 3-element array")
+    msg = roslibpy.Message({
+        'x': arr[0],
+        'y': arr[1],
+        'z': arr[2]
+    })
+    return msg
 
 
 def point_to_numpy(msg, hom=False):
     if hom:
-        return np.array([msg.x, msg.y, msg.z, 1])
+        return np.array([msg['x'], msg['y'], msg['z'], 1])
     else:
-        return np.array([msg.x, msg.y, msg.z])
+        return np.array([msg['x'], msg['y'], msg['z']]).reshape(3, 1)
 
 
 def numpy_to_point(arr):
-    if arr.shape[-1] == 4:
-        arr = arr[..., :-1] / arr[..., -1]
+    if arr.dtype != np.float64:
+        raise ValueError("Expected a floating point array")
 
-    if len(arr.shape) == 1:
-        return Point(**dict(zip(['x', 'y', 'z'], arr)))
-    else:
-        return np.apply_along_axis(
-            lambda v: Point(**dict(zip(['x', 'y', 'z'], v))), axis=-1, arr=arr)
+    if arr.shape != (3, 1):
+        raise ValueError("Expected a 3x1 dimensional array")
+    msg = roslibpy.Message({
+        'x': arr[0],
+        'y': arr[1],
+        'z': arr[2]
+    })
+    return msg
 
 
 def quat_to_numpy(msg):
-    return np.array([msg.x, msg.y, msg.z, msg.w])
+    return np.array([msg['x'], msg['y'], msg['z'], msg['w']])
 
 
 def numpy_to_quat(arr):
-    assert arr.shape[-1] == 4
+    if arr.dtype != np.float64:
+        raise ValueError("Expected a floating point array")
 
-    if len(arr.shape) == 1:
-        return Quaternion(**dict(zip(['x', 'y', 'z', 'w'], arr)))
-    else:
-        return np.apply_along_axis(
-            lambda v: Quaternion(**dict(zip(['x', 'y', 'z', 'w'], v))),
-            axis=-1, arr=arr)
+    if arr.shape != (4, 1):
+        raise ValueError("Expected a 4-element array")
+    msg = roslibpy.Message({
+        'x': arr[0],
+        'y': arr[1],
+        'z': arr[2],
+        'w': arr[3]
+    })
+    return msg
 
 
 def transform_to_numpy(msg):
     trans = np.array(transformations.translation_matrix(
-        [msg.translation.x, msg.translation.y, msg.translation.z]))
+        [msg['translation']['x'], msg['translation']['y'], msg['translation']['z']]))
     rot = np.array(transformations.quaternion_matrix(
-        [msg.rotation.x, msg.rotation.y, msg.rotation.z, msg.rotation.w]))
+        [msg['rotation']['x'], msg['rotation']['y'], msg['rotation']['z'], msg['rotation']['w']]))
     return np.dot(trans, rot)
 
 
 def numpy_to_transform(arr):
-    shape, rest = arr.shape[:-2], arr.shape[-2:]
-    assert rest == (4, 4)
-
-    if len(shape) == 0:
-        trans = transformations.translation_from_matrix(arr)
-        quat = transformations.quaternion_from_matrix(arr)
-
-        return Transform(
-            translation=Vector3(**dict(zip(['x', 'y', 'z'], trans))),
-            rotation=Quaternion(**dict(zip(['x', 'y', 'z', 'w'], quat)))
-        )
-    else:
-        res = np.empty(shape, dtype=np.object_)
-        for idx in np.ndindex(shape):
-            res[idx] = Transform(
-                translation=Vector3(
-                    **dict(
-                        zip(['x', 'y', 'z'],
-                            transformations.translation_from_matrix(arr[idx])))),
-                rotation=Quaternion(
-                    **dict(
-                        zip(['x', 'y', 'z', 'w'],
-                            transformations.quaternion_from_matrix(arr[idx]))))
-            )
+    if arr.dtype != np.float64:
+        raise ValueError("Expected a floating point array")
+    if arr.shape != (4, 4):
+        raise ValueError("Expected a 4x4 array")
+    # Convert the 4x4 matrix to a translation and rotation
+    x = transformations.translation_from_matrix(arr)
+    q = transformations.quaternion_from_matrix(arr)
+    msg = roslibpy.Message({
+        'translation': {
+            'x': x[0],
+            'y': x[1],
+            'z': x[2]
+        },
+        'rotation': {
+            'x': q[0],
+            'y': q[1],
+            'z': q[2],
+            'w': q[3]
+        }
+    })
+    return msg
 
 
 def pose_to_numpy(msg):
     return np.dot(
-        transformations.translation_matrix([msg.position.x, msg.position.y, msg.position.z]),
-        transformations.quaternion_matrix([msg.orientation.x, msg.orientation.y, msg.orientation.z, msg.orientation.w])
+        transformations.translation_matrix([msg['position']['x'], msg['position']['y'], msg['position']['z']]),
+        transformations.quaternion_matrix(
+            [msg['orientation']['x'], msg['orientation']['y'], msg['orientation']['z'], msg['orientation']['w']])
     )
 
 
 def numpy_to_pose(arr):
-    shape, rest = arr.shape[:-2], arr.shape[-2:]
-    assert rest == (4, 4)
-
-    if len(shape) == 0:
-        trans = transformations.translation_from_matrix(arr)
-        quat = transformations.quaternion_from_matrix(arr)
-
-        return Pose(
-            position=Point(**dict(zip(['x', 'y', 'z'], trans))),
-            orientation=Quaternion(**dict(zip(['x', 'y', 'z', 'w'], quat)))
-        )
-    else:
-        res = np.empty(shape, dtype=np.object_)
-        for idx in np.ndindex(shape):
-            res[idx] = Pose(
-                position=Point(
-                    **dict(
-                        zip(['x', 'y', 'z'],
-                            transformations.translation_from_matrix(arr[idx])))),
-                orientation=Quaternion(
-                    **dict(
-                        zip(['x', 'y', 'z', 'w'],
-                            transformations.quaternion_from_matrix(arr[idx]))))
-            )
+    if arr.dtype != np.float64:
+        raise ValueError("Expected a floating point array")
+    if arr.shape != (4, 4):
+        raise ValueError("Expected a 4x4 array")
+    # Convert the 4x4 matrix to a translation and rotation
+    x = transformations.translation_from_matrix(arr)
+    q = transformations.quaternion_from_matrix(arr)
+    msg = roslibpy.Message({
+        'position': {
+            'x': x[0],
+            'y': x[1],
+            'z': x[2]
+        },
+        'orientation': {
+            'x': q[0],
+            'y': q[1],
+            'z': q[2],
+            'w': q[3]
+        }
+    })
+    return msg
